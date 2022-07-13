1aa50e5f25cc3feb7bff4790a5e55b4d48642e82
Yundukki <lch001@ucsd.edu>
2022-07-12 20:30:59 -0700

    hi

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   setFontSize: {
-    fontSize: '30px'
+    fontSize: '20px'
   },
   box: {
     backgroundColor: '#86ebd3',
