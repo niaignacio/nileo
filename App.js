5505fc1300c1c182bb5dbadd463dea54ea1ec842
Nia Ignacio <nignacio@usc.edu>
2022-07-12 20:39:45 -0700

    nia branch

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
       <Text style = {styles.setFontSize}>Hello, let's take a look around!</Text>
   
       <View style ={styles.box}>
-      <Text></Text>
+      <Text> Nia Branch </Text>
 
       </View>
       <StatusBar style="auto" />
